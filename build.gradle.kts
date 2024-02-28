9321543defed8998b3a096392b521d00cba080ef
Kirill Lukyanov <kirs2000@mail.ru>
2024-02-28 09:55:25 +0300

    FizzBuzzProcessor and tests added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+
 }
 
 group = "ru.lukyanov"
@@ -12,7 +13,17 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:1.18.30")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.9")
+
+
+
 }
+task(println("some text goes to Console").toString())
 
 tasks.test {
     useJUnitPlatform()
