585bc4bd28453fbd0b3e7f54889f796b511c6bb1
Kirill Lukyanov <kirs2000@mail.ru>
2024-03-25 13:41:38 +0300

    build.gradle.kts config modified

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.4"
 
 }
 
@@ -8,6 +9,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    google()
+    flatDir {
+        dirs("mytestrepo")
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    }
 }
 
 dependencies {
@@ -19,11 +30,22 @@ dependencies {
 
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.9")
-
-
-
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-thymeleaf
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.2.4")
 }
-task(println("some text goes to Console").toString())
+
+task(println("some text goes to Console.\n" + System.getProperty("java.home")).toString())
+task(delete("build/docs").toString())
+
+tasks {
+    register("myTask") {
+        doLast {
+            logger.log(LogLevel.INFO, "just a test log").toString()
+        }
+    }
+}
 
 tasks.test {
     useJUnitPlatform()
--- src/main/java/ru/lukyanov/Main.java
+++ src/main/java/ru/lukyanov/Main.java
@@ -1,5 +1,9 @@
 package ru.lukyanov;
 
+/**
+ * simple javadoc to test gradle task execution
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
